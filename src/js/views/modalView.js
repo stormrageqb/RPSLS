d47ea13b2e10c217a8c136160490747df3925a5f
stormrageqb <stormrage.qb@gmail.com>
2022-10-19 14:19:11 -0500

    Add layout and styles for reset score modal

--- src/js/views/modalView.js
+++ src/js/views/modalView.js
@@ -1,8 +1,10 @@
 class ModalView {
   _modalWindow = document.querySelector('.rule-window');
+  _resetWindow = document.querySelector('.reset-window');
   _overlay = document.querySelector('.overlay');
   _btnOpen = document.querySelector('.btn-rules');
   _btnClose = document.querySelector('.btn-close-modal');
+  _btnReset = document.querySelector('.btn-reset');
 
   constructor() {
     this._addHandlerShowModal();
@@ -14,8 +16,14 @@ class ModalView {
     this._modalWindow.classList.toggle('hidden');
   }
 
+  toggleReset() {
+    this._overlay.classList.toggle('hidden');
+    this._resetWindow.classList.toggle('hidden');
+  }
+
   _addHandlerShowModal() {
     this._btnOpen.addEventListener('click', this.toggleModal.bind(this));
+    this._btnReset.addEventListener('click', this.toggleReset.bind(this));
   }
 
   _addHandlerHideModal() {
